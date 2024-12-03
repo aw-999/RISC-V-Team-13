a36a5aa9f3ae15cf7632a53dbea6e2ed14c48464
aw-999 <masteraw.999@gmail.com>
2024-12-03 16:34:40 +0000

    moved Unit Tests into testing folder

---
+++ tb/tests/Unit-tests/Register-file/obj_dir/Vregfile___024root__DepSet_h357c9822__0__Slow.cpp
@@ -0,0 +1,67 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vregfile.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vregfile___024root.h"
+
+VL_ATTR_COLD void Vregfile___024root___settle__TOP__0(Vregfile___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vregfile__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vregfile___024root___settle__TOP__0\n"); );
+    // Body
+    vlSelf->regfile__DOT__register_array[0U] = 0U;
+    vlSelf->A0 = vlSelf->regfile__DOT__register_array
+        [0xaU];
+    vlSelf->DOut1 = vlSelf->regfile__DOT__register_array
+        [vlSelf->A1];
+    vlSelf->DOut2 = vlSelf->regfile__DOT__register_array
+        [vlSelf->A2];
+}
+
+VL_ATTR_COLD void Vregfile___024root___eval_initial(Vregfile___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vregfile__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vregfile___024root___eval_initial\n"); );
+    // Body
+    vlSelf->__Vclklast__TOP__clk = vlSelf->clk;
+}
+
+VL_ATTR_COLD void Vregfile___024root___eval_settle(Vregfile___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vregfile__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vregfile___024root___eval_settle\n"); );
+    // Body
+    Vregfile___024root___settle__TOP__0(vlSelf);
+    vlSelf->__Vm_traceActivity[1U] = 1U;
+    vlSelf->__Vm_traceActivity[0U] = 1U;
+}
+
+VL_ATTR_COLD void Vregfile___024root___final(Vregfile___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vregfile__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vregfile___024root___final\n"); );
+}
+
+VL_ATTR_COLD void Vregfile___024root___ctor_var_reset(Vregfile___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vregfile__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vregfile___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->clk = VL_RAND_RESET_I(1);
+    vlSelf->WE3 = VL_RAND_RESET_I(1);
+    vlSelf->write_addr = VL_RAND_RESET_I(5);
+    vlSelf->A1 = VL_RAND_RESET_I(5);
+    vlSelf->A2 = VL_RAND_RESET_I(5);
+    vlSelf->WD3 = VL_RAND_RESET_I(32);
+    vlSelf->DOut1 = VL_RAND_RESET_I(32);
+    vlSelf->DOut2 = VL_RAND_RESET_I(32);
+    vlSelf->A0 = VL_RAND_RESET_I(32);
+    for (int __Vi0=0; __Vi0<32; ++__Vi0) {
+        vlSelf->regfile__DOT__register_array[__Vi0] = VL_RAND_RESET_I(32);
+    }
+    for (int __Vi0=0; __Vi0<2; ++__Vi0) {
+        vlSelf->__Vm_traceActivity[__Vi0] = VL_RAND_RESET_I(1);
+    }
+}
