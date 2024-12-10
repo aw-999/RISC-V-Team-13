2d421dd644d9ccae2c9922ac479583f3275053f5
D-Hagmann <dominik.hagmann@me.com>
2024-12-10 17:27:51 +0000

    debugging

--- tb/doit.sh
+++ tb/doit.sh
@@ -15,6 +15,10 @@ RESET=$(tput sgr0)
 passes=0
 fails=0
 
+# Google Test paths (adjust these as necessary)
+GTEST_INCLUDE_PATH="/opt/homebrew/include"  # Path to Google Test includes
+GTEST_LIB_PATH="/opt/homebrew/lib"          # Path to Google Test libraries
+
 # Handle terminal arguments
 if [[ $# -eq 0 ]]; then
     # If no arguments provided, run all tests
@@ -29,12 +33,16 @@ cd $SCRIPT_DIR
 # Wipe previous test output
 rm -rf test_out/*
 
+# Set environment variables for make
+export CXXFLAGS="-I${GTEST_INCLUDE_PATH}"  # Add Google Test include path
+export LDFLAGS="-L${GTEST_LIB_PATH} -lgtest -lgtest_main -lpthread"  # Add Google Test library path
+
 # Iterate through files
 for file in "${files[@]}"; do
     name=$(basename "$file" _tb.cpp | cut -f1 -d\-)
 
     # If verify.cpp -> we are testing the top module
-    if [ $name == "verify.cpp" ]; then
+    if [ $name == "verify_cpu.cpp" ]; then
         name="top"
     fi
 
@@ -44,8 +52,7 @@ for file in "${files[@]}"; do
                 --exe ${file} \
                 -y ${RTL_FOLDER} \
                 --prefix "Vdut" \
-                -o Vdut \
-                -LDFLAGS "-lgtest -lgtest_main -lpthread"
+                -o Vdut
 
     # Build C++ project with automatically generated Makefile
     make -j -C obj_dir/ -f Vdut.mk
@@ -64,3 +71,6 @@ done
 
 # Save obj_dir in test_out
 mv obj_dir test_out/
+
+# Print summary
+echo -e "${GREEN}Passed: $passes${RESET}"
