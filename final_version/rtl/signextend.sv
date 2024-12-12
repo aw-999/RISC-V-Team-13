2d3e047fa19cedd6a3e9ca065367188f055b7ecb
D-Hagmann <dominik.hagmann@me.com>
2024-12-12 19:44:02 +0000

    debugging

--- final_version/rtl/signextend.sv
+++ final_version/rtl/signextend.sv
@@ -9,11 +9,11 @@ logic sign; // for 2's complement
 assign sign = instrD[W-1]; // not required by extend (U / J type)
 
 always_comb case(immsrcD)
-    3'b000: immextD = {{20{sign}}, instrD[W-1:W-12]}; // I-type
-    3'b001: immextD = {{20{sign}}, instrD[W-1:W-7], instrD[W-21:W-25]}; // S-type
-    3'b010: immextD = {{19{sign}}, instrD[W-1], instrD[W-25], instrD[W-2:W-7], instrD[W-21:W-24], 1'b0}; //B-type
-    3'b011: immextD = {instrD[W-1:W-20], 12'b0}; // U-type
-    3'b100: immextD = {{11{sign}}, instrD[W-1], instrD[W-13:W-20], instrD[W-12], instrD[W-2:W-11], 1'b0}; // J-type
+    3'b000: immextD = {{20{sign}}, instrD[31:20]}; // I-type
+    3'b001: immextD = {{20{instrD[31]}}, instrD[31:25], instrD[11:7]}; // S-type
+    3'b010: immextD = {{20{instrD[31]}}, instrD[7], instrD[30:25], instrD[11:8], 1'b0}; //B-type
+    3'b011: immextD = {instrD[31:12], 12'b0}; // U-type
+    3'b100: immextD = {{12{instrD[31]}}, instrD[19:12], instrD[20], instrD[30:21], 1'b0}; // J-type
     default: immextD = instrD;
 endcase
 
