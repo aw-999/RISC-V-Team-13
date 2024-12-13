569aba531e0c23e1b9b292004e3fba702d89542a
SImoNJess <haiyi.yu23.imperial.ac.uk>
2024-12-13 03:27:21 +0000

    random start time reduced & align main to main_top

--- rtl/F1test.cpp
+++ rtl/F1test.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv, char **env)
     Verilated::traceEverOn(true);
     VerilatedVcdC *tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
-    tfp->open("main.vcd");
+    tfp->open("main_top.vcd");
 
     // init Vbuddy
     if (vbdOpen() != 1)
